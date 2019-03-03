663d737a4510c68a1dc8ea59aaf0edf938d5aa15
Mariana Garman <garman.mariana@gmail.com>
2019-03-03 21:49:51 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,14 @@
+def fizzbuzz(integer)
+  if (integer % 3 == 0 && integer % 5 == 0)
+    return "FizzBuzz"
+    elsif (integer % 3 == 0) 
+    return "Fizz"
+    elsif (integer % 5 == 0)
+    return "Buzz"
+  end 
+end 
+
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+#Define helper_method that takes (integer) argument
+#
